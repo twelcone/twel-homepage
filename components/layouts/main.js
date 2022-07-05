35b029dba3123cd7b1d16d538ea1d51c2c9b7cd1
twelcone <twelcone@github.com>
2022-07-05 11:47:42 +0700

    Integrate with Chakra-ui

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,13 +1,14 @@
 import Head from 'next/head'
 import { Box, Container } from '@chakra-ui/react'
 
-const Main = ({ children, router }) => (
-  return(
+const Main = ({ children, router }) => {
+  return (
     <Box as="main" pb={8}>
       <Head>
-        <meta name="viewport" content="width=device-width, intial-scale=1" />
-	<title>Twel Vu - Homepage </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+	<title>Twel Vu - Homepage</title>
       </Head>
+
       <Container maxW="container.md" pt={14}>
         {children}
       </Container>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,22 @@
+import { Container, Box, Heading } from '@chakra-ui/react'
+
 const Page = () => {
-  return <div>Hello</div>
-}
+  return (
+    <Container>    
+      <Box borderRadius="lg" bg="red" p={3} mb={6} align='center'>
+        Hello, I'm a college student at University of Information Technology - VNUHCM
+      </Box>
+
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+	  <Heading as="h2" variant="page-title">
+	    Twel Vu
+      	  </Heading>
+	  <p>Research Intern | Student </p>
+	</Box>
+      </Box>
+    </Container>
+  )
+} 
 
 export default Page
