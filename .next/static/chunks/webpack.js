bfcdf649a6ace88011acd6ebbe298ba38b787c81
twelcone <twelcone@github.com>
2022-07-06 22:49:33 +0700

    Change font

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ff5dec1cbc1ccf24"; }
+/******/ 		__webpack_require__.h = function() { return "0a8cad175f417e0d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
